f7f7ba7625ba26084100e3036e970d987443b316
Justin Merryman <justinmerryman@yahoo.com>
2021-01-12 16:21:48 -0600

    Question 3 answered

--- EmployeeSQL/Data_Analysis.sql
+++ EmployeeSQL/Data_Analysis.sql
@@ -17,3 +17,16 @@ Select first_name as "First Name" ,
 	hire_date as "Hire Date"
 From Employee
 where  extract (year from hire_date) = 1986;
+
+--3. List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
+Select department_manager.dept_no as "Department Number" , 
+	department.dept_name as "Department Name",
+	department_manager.emp_no as "Managers Employee Number", 
+	employee.last_name as "Last Name", 
+	employee.first_name as "First Name" 
+from ((department_manager
+inner join employee on 
+department_manager.emp_no = employee.emp_no)
+	  inner join department on 
+	  department_manager.dept_no = department.dept_no ); 
+
