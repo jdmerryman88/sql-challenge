3f5ed0482620882f98ae57c499f3809e9f0820e1
Justin Merryman <justinmerryman@yahoo.com>
2021-01-12 16:21:13 -0600

    Question 2 answered

--- EmployeeSQL/Data_Analysis.sql
+++ EmployeeSQL/Data_Analysis.sql
@@ -10,3 +10,10 @@ inner join employee on
 (employee.emp_no = salary.emp_no);
 
 
+--2. List first name, last name, and hire date for employees who were hired in 1986.
+select * from employee;
+Select first_name as "First Name" , 
+	last_name as "Last Name" , 
+	hire_date as "Hire Date"
+From Employee
+where  extract (year from hire_date) = 1986;
